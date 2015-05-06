47c2d66a4d224de8ac179c0b8d3c7b6b5eac92fd
paola gonzalez rosas <gonzalezpaola127235@gmail.com>
2015-05-06 11:07:47 -0700

    videos 1 and 2

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+	// configuration for our php server 
+	set_time_limit(0);
+	ini_set('default_socket_timeout', 300);
+	session_start();
+
+	// make constants using defined 
+	define('elient_id', 'c73d173254d844b89d8117954f97d9ee');
+	define('elient_secret', '971766cd8c4f4af7b7a6ff36f32b68b0');
+	define('redirectURL', 'http://localhost:8888/appacademyapi/index.php');
+	define('elient_id', 'c73d173254d844b89d8117954f97d9ee');
+
+?>
+
+CLIENT INFO 
+CLIENT ID c73d173254d844b89d8117954f97d9ee
+CLIENT SECRET 971766cd8c4f4af7b7a6ff36f32b68b0
+WEBSITE URL http://localhost:8888/appacademyapi/index.php
+REDIRECT URL http://localhost:8888/appacademyapi/index.php
