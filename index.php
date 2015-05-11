0a8a6ff6e6e1e4cf898fbaada24ecd5266d982b6
paola gonzalez rosas <gonzalezpaola127235@gmail.com>
2015-05-10 18:05:25 -0700

    video 4

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	session_start();
 
 	// make constants using defined 
-	define('client_ID', 'c73d173254d844b89d8117954f97d9ee');
+	define('clientID', 'c73d173254d844b89d8117954f97d9ee');
 	define('client_Secret', '971766cd8c4f4af7b7a6ff36f32b68b0');
 	define('redirectURL', 'http://localhost:8888/appacademyapi/index.php');
 	define('ImageDirectory', 'pics/');
@@ -20,7 +20,7 @@
 <body>
 <!-- creating login for peple to go and give approvel for our web app to access their accoutn
 ater getting approval we are now going to have info -->
-	<a href="https:api.instagram/oauth/authorize/?client_id=<?php echo client_ID;?>
+	<a href="https:api.instagram/oauth/authorize/?client_id=<?php echo clientSID;?>
 	&redirec_url=<?php echo redirectURL;?>
 	&response_type=code">LOGIN</a>
 
