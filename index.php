457196b581c0f6e7d38b0b4f73df4b821aaa6f1f
paola gonzalez rosas <gonzalezpaola127235@gmail.com>
2015-05-10 18:23:55 -0700

    video 6

--- index.php
+++ index.php
@@ -10,14 +10,15 @@
 	define('redirectURL', 'http://localhost:8888/appacademyapi/index.php');
 	define('ImageDirectory', 'pics/');
 
-	if isset(($_GET['code'])) {
+	if (isset($_GET['code'])) {
 		$code = ($_GET['code']);
 		$url = 'https:api.instagram/oauth/access_token';
 		$access_token_settings = array('client_id' => client_id,
 			'client_secret' => client_Secret,
 			'grant_type' => 'authorization_code',
 			'redirect_url' => redirectURL,
-			'code' => $code)
+			'code' => $code
+			);
 	}
 
 ?>
